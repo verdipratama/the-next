fa6abf11a13ee106aa568802600f9d7db069b141
verdipratama <verdipratama@icloud.com>
2020-01-27 10:57:18 +0700

    fix deploy

---
+++ .next/precache-manifest.f430313e3a0c4f2b4415154181467a35.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903a213f2bce07ddeff8c01d4b2c53a7",
+    "url": "build-manifest.json"
+  },
+  {
+    "revision": "99914b932bd37a50b983c5e7c90ae93b",
+    "url": "react-loadable-manifest.json"
+  },
+  {
+    "revision": "fb96ae7926f5104f50f0cf1b3a23a9b5",
+    "url": "static/Tz15fvFg43BeasjyA3YUy/_buildManifest.js"
+  },
+  {
+    "revision": "62be20ed452fcddb7177",
+    "url": "static/Tz15fvFg43BeasjyA3YUy/pages/_app.js"
+  },
+  {
+    "revision": "7a1ed073bd7946a86a73",
+    "url": "static/Tz15fvFg43BeasjyA3YUy/pages/_error.js"
+  },
+  {
+    "revision": "12f276cd5c3154a88990",
+    "url": "static/Tz15fvFg43BeasjyA3YUy/pages/index.js"
+  },
+  {
+    "revision": "b29b243bfce03480f23c",
+    "url": "static/chunks/commons.6b5b42654657d9aebcbb.js"
+  },
+  {
+    "revision": "7769a45dde4b24cf5e11",
+    "url": "static/chunks/framework.74d547792b3163b4d6d2.js"
+  },
+  {
+    "revision": "77b8117d7bbfd9984a5b",
+    "url": "static/runtime/main-73c5bb40f43c6379d1c8.js"
+  },
+  {
+    "revision": "f5f90f842eb19065a2dd",
+    "url": "static/runtime/polyfills-59d32c5e0ed3dd797824.js"
+  },
+  {
+    "revision": "959f28c20c788af74930",
+    "url": "static/runtime/webpack-91b117697e716c22a78b.js"
+  }
+]);
