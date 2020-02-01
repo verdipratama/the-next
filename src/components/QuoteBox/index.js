384d7b5dd6439e66e3509d008c4c9b3724eafaa0
verdipratama <verdipratama@icloud.com>
2020-02-02 01:53:40 +0700

    Refactoring: All the code

---
+++ src/components/QuoteBox/index.js
@@ -0,0 +1,40 @@
+import { Box, Text } from '@chakra-ui/core';
+import useRandomQuotes from '../../hooks/useRandomQuotes';
+
+export default () => {
+  const { loading, data } = useRandomQuotes();
+
+  const renderQuote = () => {
+    if (loading) return <span>Loading...</span>;
+    if (!data.content) return null;
+
+    return (
+      <>
+        <h1>&quot;{data.content}&quot;</h1>
+        <span>{` - ${data.author} via Quotable`}</span>
+      </>
+    );
+  };
+
+  return (
+    <Box
+      as="main"
+      width="100%"
+      margin="auto"
+      maxWidth={['100%', '100%', '64vw']}
+      padding={['4rem 1rem', '4rem 2rem']}
+      textAlign="center"
+      bg="blue.700"
+      mt={8}
+      color="white"
+      boxShadow="xl"
+    >
+      <div>
+        <Text fontSize="28px" color="tomato" fontWeight="bold">
+          Welcome to The Next Level ... :)
+        </Text>
+      </div>
+      <Text mt={2}>{renderQuote()}</Text>
+    </Box>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import { NextSeo } from 'next-seo';
-import { Box, Text } from '@chakra-ui/core';
-import useRandomQuotes from '../hooks/useRandomQuotes';
 import Layout from '../components/Layout';
 import Header from '../components/Layout/Header';
+import QuoteBox from '../components/QuoteBox';
 
 const SEO = () => (
   <NextSeo
@@ -29,44 +28,12 @@ const SEO = () => (
 );
 
 export default () => {
-  const { loading, data } = useRandomQuotes();
-
-  const renderQuote = () => {
-    if (loading) return <span>Loading...</span>;
-    if (!data.content) return null;
-
-    return (
-      <>
-        <h1>&quot;{data.content}&quot;</h1>
-        <span>{` - ${data.author} via Quotable`}</span>
-      </>
-    );
-  };
-
   return (
     <>
       <SEO />
       <Layout>
         <Header />
-        <Box
-          as="main"
-          width="100%"
-          margin="auto"
-          maxWidth={['100%', '100%', '64vw']}
-          padding={['4rem 1rem', '4rem 2rem']}
-          textAlign="center"
-          bg="blue.700"
-          mt={8}
-          color="white"
-          boxShadow="xl"
-        >
-          <div>
-            <Text fontSize="28px" color="tomato" fontWeight="bold">
-              Welcome to The Next Level ... :)
-            </Text>
-          </div>
-          <Text mt={2}>{renderQuote()}</Text>
-        </Box>
+        <QuoteBox />
       </Layout>
     </>
   );
